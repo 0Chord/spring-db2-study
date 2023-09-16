3a14fba886698a3b797f8fe3096e053ac60a80a8
0Chord <seaweed.0chord@gmail.com>
2023-09-16 14:21:36 +0900

    feat : 추가 transaction 단일 트랜잭션 예제

---
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,75 @@
+package hello.springtx.propagation;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+	@Autowired
+	MemberService memberService;
+
+	@Autowired
+	MemberRepository memberRepository;
+
+	@Autowired
+	LogRepository logRepository;
+
+	/**
+	 * memberService : @Transactional off
+	 * memberRepository : @Transactional on
+	 * logRepository : @Transactional on
+	 */
+	@Test
+	void outerTxOff_success() {
+		//given
+		String username = "outerTxOff_success";
+		//when
+		memberService.joinV1(username);
+		//then
+		assertTrue(memberRepository.find(username).isPresent());
+		assertTrue(logRepository.find(username).isPresent());
+	}
+
+	/**
+	 * memberService : @Transactional off
+	 * memberRepository : @Transactional on
+	 * logRepository : @Transactional on Exception
+	 */
+	@Test
+	void outerTxOff_fail() {
+		//given
+		String username = "로그예외_outerTxOff_fail";
+		//when
+		assertThatThrownBy(() -> memberService.joinV1((username))).isInstanceOf(
+			RuntimeException.class);
+		//then
+		assertTrue(memberRepository.find(username).isPresent());
+		assertTrue(logRepository.find(username).isEmpty());
+	}
+
+	/**
+	 * memberService : @Transactional on
+	 * memberRepository : @Transactional off
+	 * logRepository : @Transactional off
+	 */
+	@Test
+	void singleTx() {
+		//given
+		String username = "outerTxOff_success";
+		//when
+		memberService.joinV1(username);
+		//then
+		assertTrue(memberRepository.find(username).isPresent());
+		assertTrue(logRepository.find(username).isPresent());
+	}
+}
